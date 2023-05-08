23e00e7adb375fd6ee2cd64e3c326f85078160f0
[Mayank Bhadaraka] <[Mayankbhadraka@lucentinnovation.com]>
2023-05-08 16:40:04 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,198 @@
+const logotext = "Mayank";
+const meta = {
+  title: "Mayank Bhadaraka",
+  description:
+    "I’m Mayank Bhadaraka MERN Stack Developer _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Mayank Bhadaraka",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop Website",
+  },
+  description:
+    "I am enthusiastic about designing, maintaining, and delivering high-quality single page and eCommerce apps as a full stack web developer with expertise in the MERN stack. My knowledge and expertise in HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB enable me to provide solid and efficient online solutions.",
+  your_img_url: "Images/Logo1.png",
+};
+
+const dataabout = {
+  title: "Know more about my self",
+  aboutme: `I am enthusiastic about designing, maintaining, and delivering high-quality single page and eCommerce apps as a full stack web developer with expertise in the MERN stack. My knowledge and expertise in HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB enable me to provide solid and efficient online solutions.
+What distinguishes me is my ability to quickly get used to new technologies, allowing me to work on any tech stack. I am a dedicated and hardworking learner that is constantly attempting to keep up with the newest web development trends and best practices.
+I think that cooperation and communication are critical to project success, and I constantly endeavor to work closely with my clients to understand their individual needs and goals. 
+I am confident in my ability to create great outcomes that meet and surpass expectations with my experience and devotion.
+I am thrilled to help you bring your idea to life, whether you are wanting to develop a new website from start, optimise an existing one, or add additional features and functionality. Let's collaborate to make something truly extraordinary!`,
+};
+const worktimeline = [
+  {
+    jobtitle: "Lucent Innovation",
+    where: "Full Stack Developer",
+    date: "2023",
+  },
+  {
+    jobtitle: "Baroda-Web",
+    where: "Full Stack Developer",
+    date: "2022",
+  },
+
+  {
+    jobtitle: "Freelancing",
+    where: "TutorPoint",
+    date: "2020",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 90,
+  },
+  {
+    name: "React",
+    value: 85,
+  },
+  {
+    name: "Node",
+    value: 82,
+  },
+  {
+    name: "Express",
+    value: 80,
+  },
+  {
+    name: "SQL",
+    value: 80,
+  },
+  {
+    name: "NoSQL",
+    value: 75,
+  },
+  {
+    name: "Python",
+    value: 70,
+  },
+];
+
+const services = [
+  {
+    title: "Front-End Developer",
+    description:
+      "I specialize in creating user-friendly and visually appealing websites using HTML, CSS, and JavaScript. I can help you bring your vision to life and ensure that your website looks great on all devices.",
+  },
+  {
+    title: "Back-End Developer",
+    description:
+      " I am experienced in building server-side applications using Node.js, PHP, and other technologies. I can help you build secure and scalable web applications that meet your business needs.",
+  },
+  {
+    title: "Full Stack Developer",
+    description:
+      " I have extensive experience in both front-end and back-end development, allowing me to create end-to-end solutions for your web development needs. Whether you need a simple website or a complex web application, I can help you deliver results.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://th.bing.com/th/id/R.4977fd7c7bf45c82c71c3eb3a7df1408?rik=ciIKvB9EEsCGEA&riu=http%3a%2f%2finkbotdesign.com%2fwp-content%2fuploads%2f2014%2f05%2fChat-App-Logo.png&ehk=Jogv4DO2%2fzg2xzsyBqam22Dr4%2fEzDuhUK%2b%2fYEVt3OLo%3d&risl=&pid=ImgRaw&r=0",
+    description: "Chat Application With MERN Stack with Socket Server.",
+    link: "https://chatapp-sitr.onrender.com",
+  },
+  {
+    img: "https://i.pinimg.com/originals/de/1e/70/de1e7040e596e2ddba00140711793234.png",
+    description:
+      "Designed and developed a full-stack web application of QUOTE using ReactJS, NodeJS, ExpressJS, and MongoDB with authentication and authorization.",
+    link: "https://quote-shms.onrender.com/",
+  },
+  
+  {
+    img: "https://clipground.com/images/ecommerce-logo-png-5.png",
+    description: "Worked with Ecommerce platform  like Shopify and Bigcommerce",
+    link: "https://github.com/mayankbhadaraka/Shopify-Auth",
+  },
+  {
+    img: "https://cloud-elements.com/wp-content/uploads/2018/12/shopify-logo-600.jpg",
+    description:
+    "Created Shopify Application to manage customers and products.",
+    link: "https://github.com/mayankbhadaraka/Shopify-Admin",
+  },
+  {
+    img: "https://i.pinimg.com/originals/f9/d9/30/f9d93057dcffce913a699a889b8c83bd.jpg",
+    description:
+      "Designed a web application to make a list of tasks you want to do.In this application you can add and delete your task after you are done with that.",
+    link: "https://what-you-want-to-do--todo.web.app/",
+  },
+  {
+    img: "https://th.bing.com/th/id/R.1bab3ddffe45344bd1618cd60f723681?rik=w6iJ93%2f4JdSA9Q&riu=http%3a%2f%2fcareers.jobvite.com%2fcfds%2fpng%2f001-computer-1.png&ehk=MvMPgU2zig8Hq9QBvmlGISDh3yT43PQt8fxEyS%2bFc%2fY%3d&risl=&pid=ImgRaw&r=0",
+    description: "Created Admin Dashboard To manage other Admin's permission.",
+    link: "https://github.com/mayankbhadaraka/AdminDashboard",
+  },
+  {
+    img: "https://thumbs.dreamstime.com/b/atm-face-recognition-concept-man-using-cash-machine-unlocking-account-secure-facial-biometric-data-scan-authentication-modern-192398267.jpg",
+    description:
+    "Cardless atm withdrawal system using facedetection. I have Used python face recognition and opencv library to detect face and used tkinter to make gui of atm and data base designed in sql.",
+    link: "https://github.com/mayankbhadaraka/ATM-Withdwal",
+  },
+  {
+    img: "https://miro.medium.com/max/1024/1*E5rexDhotCqksxqT4LKwyQ.jpeg",
+    description:
+      "Real time Face recognition attendance system using python module open cv and face recognition.",
+    link: "https://github.com/mayankbhadaraka/Attendance-Monitoring",
+  },
+  {
+    img: "https://th.bing.com/th/id/OIP.kqWW6rBtQAaGzVQeaBRbOwHaE8?pid=ImgDet&rs=1",
+    description: "Portfolio Website using ReactJs and Bootstrap",
+    link: "https://mayankbhadaraka.netlify.app/",
+  },
+  {
+    img: "https://blog.logrocket.com/wp-content/uploads/2020/12/next.js.png",
+    description:
+      "Created simply Website to manage users like Admin Dashboard using NextJs.",
+    link: "https://github.com/mayankbhadaraka/NextJs",
+  },
+
+  {
+    img: "https://th.bing.com/th/id/OIP.igIv7s_88Zvf5OVcBsONAgHaE8?pid=ImgDet&rs=1",
+    description:
+      "Created keyboard using Embedded C for circuit design I used protues 8 and for code i used atmel studio and microcontroller used is AtMega32.",
+    link: "https://github.com/mayankbhadaraka/Embadded-C-Hands-on",
+  },
+  {
+    img: "https://cdn.devdojo.com/images/january2021/html-for-beginners.jpg",
+    description:
+      "Designed Web Application For HTML beginners to learn basics of HTML",
+    link: "https://github.com/mayankbhadaraka/HTML-Beginer",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "mayank042002@gmail.com",
+  YOUR_FONE: "7016992524",
+  description:
+    "Thank you for your interest in getting in touch with me. Please use form to send me a message, and I'll do my best to respond as soon as possible. Whether you have a question, feedback, or a business proposal, I'm always happy to hear from you. Your privacy is important to me, and I will only use your contact information to reply to your message. So, please fill out the form with your details and your message, and I'll be in touch soon. Thank you again for your interest!",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_jm2xc97",
+  YOUR_TEMPLATE_ID: "template_jf2mkpf",
+  YOUR_USER_ID: "GIGBbNgYpdwkqbI9A",
+};
+
+const socialprofils = {
+  github: "https://github.com/mayankbhadaraka",
+  facebook: "https://www.facebook.com/mayank.bhadaraka.1",
+  linkedin: "https://www.linkedin.com/in/mayankbhadaraka/",
+  twitter: "https://twitter.com/Myk_ahir23",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
